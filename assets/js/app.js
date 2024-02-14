f40a3115dc0fa467ca3551bfb48609cf12a7b5cd
bo-nicolaisen <bni@techcollege.dk>
2024-02-14 09:19:14 +0100

    vievCode

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,13 @@
 
-
-
-
-
-
+// globals
+const myFeaturedElement = document.getElementById('featuredProducts');
+let myProducts
 
 GetProductData()
 
-
 // model
 function GetProductData() {
-    fetch('https://dummyjson.com/products')
+    fetch('https://dummyjson.com/products?limit=100')
         .then((result) => {
 
             return result.json();
@@ -23,35 +20,91 @@ function GetProductData() {
 }
 
 
-
-
 // controller
 function ProductsRecived(productData) {
 
     //console.log(productData)
 
-    let myProducts = productData.products
-
-
+    myProducts = productData.products
 
     let myFeaturedProducts = [];
 
-    myFeaturedProducts.push(myProducts[6], myProducts[9], myProducts[23])
-
+    myFeaturedProducts.push(myProducts[12], myProducts[29], myProducts[19])
     //console.log(myFeaturedProducts);
 
     CreateProductView(myFeaturedProducts)
-
+    //CreateProductView(myProducts)
 }
+
+
+
 
 
 // view code
 function CreateProductView(myCards) {
-    console.log(myCards);
+    //console.log(myCards);
+
+    myCards.forEach(product => {
+        console.log(product);
+
+
+        let myHTML = `<figure onclick="ProductCallback(${product.id})" ><h2>${product.title}</h2><img src="${product.thumbnail}"><h3>PRIS: ${product.price} rabat: ${product.discountPercentage}</h3></figure>`
+
+
+        myFeaturedElement.innerHTML += myHTML
+    });
+}
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function ProductCallback(myId) {
+    console.log(myId);
+    let myClickedProduct = null;
+    myProducts.forEach(product => {
+        if (product.id == myId) {
+            myClickedProduct = product;
+        }
+    }
+    )
+    console.log(myClickedProduct);
+    clearApp();
+    buildProduct(myClickedProduct)
+}
+
+function buildProduct(product) {
+
+    let myHTML = `<figure onclick="ProductCallback(${product.id})" ><h2>${product.title}</h2><img src="${product.thumbnail}"><h3>PRIS: ${product.price}</h3></figure>`
+
+
+    myFeaturedElement.innerHTML = myHTML
+}
+
+
+function clearApp() {
 
 }
+
 
 
