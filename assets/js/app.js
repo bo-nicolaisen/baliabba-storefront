bf9aa1db1a3cb92c2e7dc962d7666252b8fd0cc8
bo-nicolaisen <bni@techcollege.dk>
2024-02-15 10:19:04 +0100

    vertical slice with functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,54 @@
 
 // globals
-const myFeaturedElement = document.getElementById('featuredProducts');
-let myProducts
+const productSection = document.getElementById('featuredProducts');
+const navElenent = document.getElementById('navigation');
+
+let myProducts = null
+
 
 GetProductData()
+GetCategoryData()
 
 // model
 function GetProductData() {
+
     fetch('https://dummyjson.com/products?limit=100')
+
         .then((result) => {
+            //console.log(result);
+            return result.json()
+        }
+        )
 
-            return result.json();
-        })
         .then((json) => {
-
-            ProductsRecived(json);
-
+            ProductsRecived(json)
         });
+
 }
 
+// model
+function GetCategoryData() {
+
+    fetch('https://dummyjson.com/products/categories')
+
+        .then((result) => {
+            //console.log(result);
+            return result.json()
+        }
+        )
+
+        .then((json) => {
+            Categoriesrecived(json)
+        });
+
+}
+
+
+function Categoriesrecived(categoryData) {
+    // temporary code
+    //console.log(categoryData);
+    CreateNavigation(categoryData)
+}
 
 // controller
 function ProductsRecived(productData) {
@@ -29,31 +59,67 @@ function ProductsRecived(productData) {
 
     let myFeaturedProducts = [];
 
-    myFeaturedProducts.push(myProducts[12], myProducts[29], myProducts[19])
+    myFeaturedProducts.push(myProducts[8], myProducts[29], myProducts[19])
     //console.log(myFeaturedProducts);
 
     CreateProductView(myFeaturedProducts)
-    //CreateProductView(myProducts)
+    // CreateProductView(myProducts)
 }
 
 
+
+
+//view code
+function CreateNavigation(categoryArray) {
+    // temporary code
+    let myHtml = ""
+    myHtml += `<div onclick="NavigationCallback('all')" >All</div>`
+    categoryArray.forEach((category) => {
+        myHtml += `<div onclick="NavigationCallback('${category}')" >${category}</div>`
+
+    });
+
+
+    navElenent.innerHTML = myHtml
+}
+
+function NavigationCallback(myCategory) {
+
+    let productByCategory = []
+    if (myCategory === 'all') {
+        CreateProductView(myProducts);
+    }
+    else {
+        myProducts.forEach(product => {
+
+            if (product.category == myCategory) {
+                productByCategory.push(product)
+            }
+        }
+        )
+
+        CreateProductView(productByCategory);
+
+    }
+
+}
 
 
 
 // view code
 function CreateProductView(myCards) {
     //console.log(myCards);
-
+    clearApp()
 
     myCards.forEach(product => {
-        console.log(product);
+        // console.log(product);
 
 
         let myHTML = `<figure onclick="ProductCallback(${product.id})" ><h2>${product.title}</h2><img src="${product.thumbnail}"><h3>PRIS: ${product.price} rabat: ${product.discountPercentage}</h3></figure>`
 
 
-        myFeaturedElement.innerHTML += myHTML
-    });
+        productSection.innerHTML += myHTML
+    })
 }
 
 
@@ -63,48 +129,61 @@ function CreateProductView(myCards) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// controller
 function ProductCallback(myId) {
+
+
+
     console.log(myId);
-    let myClickedProduct = null;
+    let myClickedProduct = null
+
+
     myProducts.forEach(product => {
+
         if (product.id == myId) {
-            myClickedProduct = product;
+            myClickedProduct = product
         }
     }
     )
-    console.log(myClickedProduct);
-    clearApp();
-    buildProduct(myClickedProduct)
+
+    if (myClickedProduct == null) {
+        // ingen produkt
+        alert('no product')
+    }
+    else {
+        // produkt
+        console.log(myClickedProduct)
+        clearApp();
+        buildProduct(myClickedProduct)
+
+    }
+
+
+
 }
 
+
+
+//view code
 function buildProduct(product) {
 
-    let myHTML = `<figure onclick="ProductCallback(${product.id})" ><h2>${product.title}</h2><img src="${product.thumbnail}"><h3>PRIS: ${product.price}</h3></figure>`
+    let myHTML = `<figure class="productDetails" onclick="GetProductData()" ><h2>${product.title}</h2>
+  
+    <img src="${product.images[0]}">
+    <img src="${product.images[2]}">
+    <img src="${product.images[3]}">
+    <h3>PRIS: ${product.price}</h3>
+    <p>${product.description}</p>
+    </figure>
+    `
 
 
-    myFeaturedElement.innerHTML = myHTML
+    productSection.innerHTML = myHTML
 }
 
-
+// view code
 function clearApp() {
-
+    productSection.innerHTML = ""
 }
 
 
